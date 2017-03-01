08a297b9c0022b0c66c398831a555300abb2ca05
Wang pei <519875872@qq.com>
2017-03-02 00:03:29 +0800

    server case sensitive

--- controllers/RecordController.js
+++ controllers/RecordController.js
@@ -1,4 +1,4 @@
-const recordService = require('../service/recordService');
+const recordService = require('../service/RecordService');
 const bookService = require('../service/BookService');
 
 module.exports = {
