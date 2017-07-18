dc9d507afd21d155899fae436ec786a18acef94a
Wang pei <519875872@qq.com>
2017-07-18 10:48:44 +0800

    resolve cors

--- src/middlewares/rest.js
+++ src/middlewares/rest.js
@@ -1,23 +1,24 @@
 module.exports = {
   APIError: function (code, message) {
-     this.code = code || 'internal:unknown_error';
-     this.message = message || '';
+    this.code = code || 'internal:unknown_error';
+    this.message = message || '';
   },
-  restify: ()=>{
-    return async (ctx, next) => {
-      ctx.rest = (data) =>{
+  restify: ()=> {
+    return async(ctx, next) => {
+      ctx.rest = (data) => {
+        ctx.response.set('Access-Control-Allow-Origin', '*');
         ctx.response.type = 'application/json';
         ctx.response.body = data;
       }
       try {
-          await next();
+        await next();
       } catch (e) {
-          ctx.response.status = 400;
-          ctx.response.type = 'application/json';
-          ctx.response.body = {
-            code: e.code || 'internal:unknown_error',
-            message:e.message || ''
-          };
+        ctx.response.status = 400;
+        ctx.response.type = 'application/json';
+        ctx.response.body = {
+          code: e.code || 'internal:unknown_error',
+          message: e.message || ''
+        };
       }
     }
   }
